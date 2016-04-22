06244409400f61d14519571cfedc60b49ca29aa0
Ben Lerner <benlerner95@gmail.com>
2016-04-22 00:18:41 -0400

    attempting to normalize data

--- main.py
+++ main.py
@@ -3,5 +3,30 @@ import numpy as np
 
 csv_file_obj = csv.reader(open('data/data.csv', 'r'))
 header = next(csv_file_obj)
-data = np.array([row for row in csv_file_obj])
+# convert the data
+raw_data = []
+for row in csv_file_obj:
+    data_row = []
+    for element in row:
+        # element can be a float, int, string, or None.
+        # the float() cast will work on both integers and floats. However the int()
+        # cast will only work on ints; if element is a float, it will fail. So,
+        # we attempt to cast element as an int:
+        try:
+            data = int(element)
+        except ValueError:
+            pass
+        # if that didn't work, then the element may yet be a float
+        try:
+            data = float(element)
+        # if that still didn't work, then the data must be either a string or None.
+        # TODO: possibly do something about None-esque values, either deleting rows
+        # or using averages instead
+        except ValueError:
+            data = element
+
+        data_row.append(data)
+    raw_data.append(data_row)
+
+data = np.array(raw_data)
 print(data)
