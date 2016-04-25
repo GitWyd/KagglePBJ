b6cdac8d295432a72393d37b6d5d41adfec4b793
GitWyd <pmw2125@columbia.edu>
2016-04-25 06:52:00 -0400

    updated: removed calculation of testing error on quiz dataset

--- philsKilgorythm.py
+++ philsKilgorythm.py
@@ -7,6 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import OrthogonalMatchingPursuit
+from sklearn.linear_model import Perceptron 
 from sklearn.ensemble import VotingClassifier
 import numpy
 from cleandata import get_data
@@ -14,7 +15,8 @@ from cleandata import store_data
 from cleandata import store_csv
 
 def main():
-    train_size = 1500
+    train_size = 5000
+    val_size = 32000
     data = get_data('data') 
     X = data[0:train_size,0:-1] 
     y = [lbl for lbl in data[0:train_size,-1]]
@@ -23,25 +25,30 @@ def main():
     # Training classifier
     # declaring weights
     w = [1,1,1] 
-    clf1 = DecisionTreeClassifier(max_depth=6)
-    clf2 = KNeighborsClassifier(n_neighbors=7)
+    clf1 = DecisionTreeClassifier(max_depth=5)
+    clf2 = KNeighborsClassifier(n_neighbors=5)
     clf3 = SVC(kernel='rbf', probability=True)
     clf4 = OrthogonalMatchingPursuit(n_nonzero_coefs=None, tol=None, fit_intercept=True, normalize=True, precompute='auto')
+    clf5 = Perceptron(penalty=None, alpha=0.0001, fit_intercept=True, n_iter=6, shuffle=True, verbose=0, eta0=1.0, n_jobs=-1, random_state=0, class_weight=None, warm_start=False)
     eclf = VotingClassifier(estimators=[('dt', clf1), ('knn', clf2), ('svc', clf3)], voting='soft', weights=w)
     clf1 = clf1.fit(X,y)
     clf2 = clf2.fit(X,y)
     clf3 = clf3.fit(X,y)
     clf4 = clf4.fit(X,y)
+    clf5 = clf5.fit(X,y)
     eclf = eclf.fit(X,y)
+
+    # predict & calculate training error
     y_hat = eclf.predict(X) 
     test_err = 1
     for yi, y_hati in zip(y, y_hat):
         test_err += (yi == y_hati) 
     test_err /= train_size
     print("train: " + str(test_err))
-    # validation data
+
+    # validation data - calculate valdiation error
     val_start = train_size
-    val_end = train_size*2
+    val_end = train_size + val_size
     # get validation data set
     X_val = data[val_start:val_end,0:-1] 
     y_val = [lbl for lbl in data[val_start:val_end,-1]]
@@ -59,10 +66,10 @@ def main():
     y_test = [lbl for lbl in data[:,-1]]
     y_test_hat = eclf.predict(X_test) 
     test_err = 1
-    for yi, y_hati in zip(y_test, y_test_hat):
-        test_err += (yi == y_hati) 
-    test_err /= X_test.shape[0] 
-    print("test: " + str(test_err))
+#    for yi, y_hati in zip(y_test, y_test_hat):
+#        test_err += (yi == y_hati) 
+#    test_err /= X_test.shape[0] 
+#    print("test: " + str(test_err))
     store_csv(y_test_hat, "killgorythm_prediciton")  
 main()
 '''
