7c34980c9969cef27791b4543d1bad2bbd725885
Ben Lerner <benlerner95@gmail.com>
2016-05-04 05:38:47 -0400

    fixed the seenwords dictionary (should have had a list of the values for each feature instead of resetting itself for each feature), made sure that the quiz data was one-hot-encoded-prepared (strings --> ints) only for the features that were categorical, made sure that the quiz data was transformed and NOT fitted to the one hot encoder, made sure that the prediction labels were ints and not floats

--- cleandata.py
+++ cleandata.py
@@ -12,12 +12,12 @@ def get_data(datafile):
     # doesn't seem like it does anything
     # convert the data
     data = transform_data(csv_file_obj)
-    
+
     # Handle quiz data request
-    if datafile == 'quiz': 
+    if datafile == 'quiz':
         csv_file_obj_quiz = csv.reader(open('data/'+ datafile +'.csv', 'r'))
         quiz_data = transform_data(csv_file_obj_quiz)
-    ''' 
+    '''
     raw_data = []
     for row in csv_file_obj:
         data_row = []
@@ -46,62 +46,74 @@ def get_data(datafile):
     # turn categorical data (strings) into integers.
     # also, find out which vectors are categorical
 
-    # a vector of booleans which determines if the corresponding feature
+    # a vector of types which determines if the corresponding feature
     # in our data is categorical
     feature_types = []
     # for every feature except for the labels
+    seenWords = {}
     for j in range(len(data[0])-1):
         counter = 0
         # a dictionary containing all the attributes seen thus far
-        seenWords = {}
         feature_types.append(type(data[0][j]))
 
         # if not categorical
         if not isinstance(data[0][j], str):
+            # TODO: fix scaling issues
             # do scaling if float values
-            if isinstance(data[0][j], float):
-                ss = StandardScaler()
-                # data[:,j] = ss.fit_transform(data[:,j].reshape(-1, 1).T)
-                data[:,j] = ss.fit_transform(data[:,j])
-                # account for quiz_data prep
-                if datafile is 'quiz' and (j <= len(quiz_data)):
-                   quiz_data[:,j] = ss.fit_transform(quiz_data[:,j])
+
+            # if isinstance(data[0][j], float):
+            #     ss = StandardScaler()
+            #     # data[:,j] = ss.fit_transform(data[:,j].reshape(-1, 1).T)
+            #     data[:,j] = ss.fit_transform(data[:,j])
+            #     # account for quiz_data prep
+            #     if datafile is 'quiz' and (j <= len(quiz_data)):
+            #        quiz_data[:,j] = ss.fit_transform(quiz_data[:,j])
             continue
         # mark the feature as being "categorical" for later use in the one hot encoder
+        seenWords[j] = {}
         for i in range(len(data)):
             # for a new attribute
-            if data[i][j] not in seenWords:
-                seenWords.update({data[i][j]:counter})
+            if data[i][j] not in seenWords[j]:
+                # seenWords.update({data[i][j]:counter})
+                seenWords[j][data[i][j]] = counter
                 # replace the data with a number representing that attribute
-                data[i][j]  = counter
+                data[i][j] = counter
                 counter += 1
             else:
                 # just use the number that represents a previously-seen attribute
-                data[i][j]=seenWords[data[i][j]]
-        ''' Same as above just for quiz_data 
+                data[i][j]=seenWords[j][data[i][j]]
+        ''' Same as above just for quiz_data
             (Assumes that all words have already been seen in test data)
         '''
-        # mark the feature as being "categorical" for later use in the one hot encoder
-    if datafile is 'quiz':
-        for j in range(len(quiz_data[0])-1):
-            for i in range(len(quiz_data)):
-                quiz_data[i][j]=seenWords[quiz_data[i][j]]
+    categorical_feats = [i for i in range(len(feature_types)) if feature_types[i] == type('s')]
 
-    categorical_features = [i for i in range(len(feature_types)) if feature_types[i] == type('s')]
+    if datafile is 'quiz':
+        # for j in range(len(quiz_data[0])-1):
+        for j in categorical_feats:
+            for i in range(len(quiz_data)):
+                # TODO take out this if statement. Shouldn't be necessary if running on the full data set
+                if quiz_data[i][j] in seenWords[j]:
+                    quiz_data[i][j] = seenWords[j][quiz_data[i][j]]
+                else:
+                    quiz_data[i][j] = 0
+
     # TODO: although it may be unnecessary
     # use a line to fill in any NaNs in the data with the mean
 
     # do the one hot encoding
     # TODO: this makes everything into a float. May want to change this?
-    enc = OneHotEncoder(categorical_features=categorical_features, dtype=np.int32)
+    # TODO: fix the handle_unknown stuff
+    enc = OneHotEncoder(categorical_features=categorical_feats, dtype=np.int32)
     one_hot_data = enc.fit_transform(data).toarray()
-    
+
     # one-hot-encode and return one_hot_quiz_data
     if datafile is 'quiz':
-        fitted_quiz_data = enc.fit_transform(quiz_data) 
+        fitted_quiz_data = enc.transform(quiz_data)
         one_hot_quiz_data = fitted_quiz_data.toarray()
         return one_hot_quiz_data
+
     return one_hot_data
+
 def transform_data(csv_file_obj):
     # doesn't seem like it does anything
     # convert the data
@@ -126,6 +138,7 @@ def transform_data(csv_file_obj):
         raw_data.append(data_row)
     data = np.array(raw_data, dtype=object)
     return data
+
 def store_data(datafile, data):
     filename = "data/" + datafile
     for j in range(len(data[0])):
@@ -152,4 +165,4 @@ def store_csv(y_hat, filename):
     file = open(outfile, 'w+')
     file.write("Id,Prediction\n")
     for i, yi in enumerate(y_hat):
-        file.write(str(i) + "," + str(yi) + "\n")
+        file.write(str(i) + "," + str(int(yi)) + "\n")
